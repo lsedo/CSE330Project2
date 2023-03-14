5c8b4734c921dff1caa679bbeb936a955e21a0ee
sg732 <103005236+sg732@users.noreply.github.com>
2023-03-14 15:20:14 -0700

    Update producer_consumer.c

--- producer_consumer.c
+++ producer_consumer.c
@@ -1,1 +1,13 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/sched.h>
+#include <linux/sched/signal.h>
+#include <linux/semaphore.h>
+#include <linux/kthread.h>
+#include <linux/init.h>
+#include <linux/proc_fs.h>
+#include <linux/ktime.h>
+#include <linux/time.h>
+#include <linux/timer.h>
+#include <linux/slab.h>
 
