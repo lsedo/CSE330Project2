a71a4a35dea9c1deb43e30b8f766a53bcab2052e
Ellis Totman <89368259+etotman00@users.noreply.github.com>
2023-03-20 02:03:09 -0700

    Update producer_consumer.c

--- producer_consumer.c
+++ producer_consumer.c
@@ -11,18 +11,17 @@
 #include <linux/slab.h>
 
 // typedef unsigned 64 bits for timekeeping
-typedef unsigned long ul_64;
+typedef unsigned long long u64;
 
 //Create 4 required module parameters
 static int buffSize = 5; 
 static int prod = 1; 
 static int cons = 1; 
 static int uuid = 1000;
-
-module_param(buffSize, int, 0644);
-module_param(prod, int, 0644);
-module_param(cons, int, 0644);
-module_param(uuid, int, 0644);
+module_param(buffSize, int, 0660);
+module_param(prod, int, 0660);
+module_param(cons, int, 0660);
+module_param(uuid, int, 0660);
 
 //Create required semaphores
 static struct semaphore mutex, full, empty;
@@ -30,19 +29,16 @@ static struct semaphore mutex, full, empty;
 // task_struct pointers
 struct task_struct **buffer = NULL;
 struct task_struct **consumer_list = NULL;
-struct task_struct **producer_list = NULL;
+struct task_struct *producer_thread = NULL;
 
 // Timer and counter to measure the consumer time
-static ul_64 total_runtime = 0;
+static u64 total_runtime = 0;
 static int consumer_count = 0;
-
-// Count the number of items produced
-static int producer_count = 0;
 
 // Producer thread - finite loop
 static int producer_func(void *args) {
   struct task_struct *task;
-  int empty_ix = 0;
+  int producer_count = 0, empty_ix = 0;
   
   for_each_process(task) {
     
@@ -50,12 +46,12 @@ static int producer_func(void *args) {
       
       // Wait on empty (Acquire semaphore lock)
       if (down_interruptible(&empty)) {
-              break;     
+              do_exit(-1);     
       }
       
       // Wait on mutex (Acquire semaphore lock)
       if (down_interruptible(&mutex)) {
-              break;
+              do_exit(-1);
       }
       
       // Produce Item- add to buffer
@@ -64,18 +60,19 @@ static int producer_func(void *args) {
       while(empty_ix < buffSize){
             if(buffer[empty_ix] == NULL){
                   buffer[empty_ix] = task;
-                  printk("[%s] Produced Item#-%d at buffer index:%d for PID:%d\n",current->comm,producer_count,empty_ix,current->pid);
                   ++producer_count;
+                  printk("[pthread] Produced Item#-%d at buffer index:%d for PID:%u\n", producer_count, empty_ix, task->pid);
+                  
                   break;
             }
             ++empty_ix;
       }
       
-      // Signal mutex (Release semaphore lock)
-      up(&mutex); 
-      
       // Signal full (Release semaphore lock)
       up(&full);
+      
+      // Signal mutex (Release semaphore lock)
+      up(&mutex); 
       
     }
     
@@ -92,19 +89,19 @@ static int consumer_func(void *args) {
   // Variables for the thread
   struct task_struct *c = NULL;
   int buffer_ix = 0;
-  ul_64 elapsed_time = 0, seconds = 0, minutes = 0, hours = 0;
+  u64 elapsed_time = 0, seconds = 0, minutes = 0, hours = 0;
   
   
   while(!kthread_should_stop()) {
     
     // Wait on full (Acquire semaphore lock)
     if (down_interruptible(&full)) {
-            break;
+            do_exit(-1);
     }
     
     // Wait on mutex (Acquire semaphore lock)
     if (down_interruptible(&mutex)) {
-            break;
+            do_exit(-1);
     }
     
     buffer_ix = 0;
@@ -118,7 +115,7 @@ static int consumer_func(void *args) {
                     buffer[buffer_ix] = NULL;
                     ++consumer_count;
                     // Calculate elapsed time of process
-                    elapsed_time = c->start_time - ktime_get_ns();
+                    elapsed_time = ktime_get_ns() - c->start_time;
                     total_runtime += elapsed_time;
                     seconds = elapsed_time / 1000000000;
                     minutes = seconds / 60;
@@ -126,17 +123,17 @@ static int consumer_func(void *args) {
                     hours = minutes / 60;
                     minutes = minutes % 60;
               // print out total time in given format
-                    printk("[%s] Consumed Item#-%d on buffer index:%d PID:%d Elapsed Time-%d:%d:%d\n",current->comm,consumer_count,buffer_ix,current->pid,(int)hours,(int)minutes,(int)seconds);
+                    printk("[cthread] Consumed Item#-%d on buffer index:%d PID:%u Elapsed Time- %.2llu:%.2llu:%.2llu\n", consumer_count, buffer_ix, c->pid, hours, minutes, seconds);
                     break;
              }
              ++buffer_ix;
-    }            
-    // Signal mutex (Release semaphore lock)
-    up(&mutex);
+    }          
     
     // Signal empty (Release semaphore lock)
     up(&empty);
     
+    // Signal mutex (Release semaphore lock)
+    up(&mutex);  
     
   }
   
@@ -151,16 +148,12 @@ int producer_consumer_init(void) {
   int i = 0;
   
   // Parameter validation
-  if ((prod != 0 && prod != 1) || cons < 0 || buffSize < 1) {
-    
-    return -1;
-
-  }
+  if ((prod != 0 && prod != 1) || cons < 0 || buffSize < 1) return -1;
   
   // Initialize semaphores
   sema_init(&empty,buffSize);
-  sema_init(&mutex,1);
   sema_init(&full,0);
+  sema_init(&mutex,1);
 
   // Buffer initialization
   buffer = (struct task_struct**)kmalloc(sizeof(struct task_struct*) * buffSize, GFP_KERNEL);
@@ -171,7 +164,7 @@ int producer_consumer_init(void) {
   }
   
   
-  // consumerList initialization
+  // consumer_list initialization
   if (cons > 0) {
     
           i = 0;
@@ -185,23 +178,14 @@ int producer_consumer_init(void) {
   
   
   // The producer_thread will be only created if prod = 1
-  if (prod > 0) {
-          i = 0;
-          producer_list = (struct task_struct**)kmalloc(sizeof(struct task_struct*) * prod, GFP_KERNEL);
-    
-          while(i < prod){
-                producer_list[i] = kthread_run(producer_func, NULL, "producer_thread");
-                ++i;
-          }
-    
-  }
+  if (prod == 1) producer_thread = kthread_run(producer_func, NULL, "pthread");
   
   // Create consumer threads
   if (cons > 0) {
           i = 0;
     
           while (i < cons) {
-                  consumer_list[i] = kthread_run(consumer_func, NULL, "consumer_thread");
+                  consumer_list[i] = kthread_run(consumer_func, NULL, "cthread");
                   ++i;
           }
   }
@@ -215,35 +199,14 @@ int producer_consumer_init(void) {
 void producer_consumer_exit(void) {
   
   // Variables for runtime calculation
-  ul_64 seconds = 0, minutes = 0, hours = 0;
+  u64 seconds = 0, minutes = 0, hours = 0;
   int count = 0;
-  
-  int i = 0;
-  
-  //  signal the semaphores for each thread to make sure they are not waiting on the semaphore
-  // for loop signals the semaphore once for each thread
-  
-  for (i = 0; i < cons; ++i) {
-    
-        up(&full);
-  }
-  
-  for (i = 0; i < cons; ++i) {
-    
-        kthread_stop(consumer_list[i]);
-  
-  }
-  
-  for (i = 0; i < prod; ++i) {
-    
-        up(&empty);
-  }
-  
-  
-  for (i = 0; i < prod; ++i) {
-    
-        kthread_stop(producer_list[i]);
-  
+ 
+  // Cease threads and release semaphores
+  while (count < cons) {
+    up(&full);
+    kthread_stop(consumer_list[count]);
+    ++count;
   }
     
   // Calculate total runtime
@@ -254,19 +217,13 @@ void producer_consumer_exit(void) {
   hours = minutes / 60;
   minutes = minutes % 60;
   
-  printk("The total elapsed time of all processes for UID %d is \t%d:%d:%d\n",uuid,hours,minutes,seconds);
+  printk("The total elapsed time of all processes for UID:%u - %.2llu:%.2llu:%.2llu\n", uuid, hours, minutes, seconds);
   
-  // kfree() for the universal pointers
+  // kfree() for two of the universal pointers. This will free the dynamically allocated memory.
   if (buffer != NULL) {
     
     kfree(buffer);
     buffer = NULL;
-  }
-  
-  if (producer_list != NULL) {
-    
-    kfree(producer_list);
-    producer_list = NULL;
   }
   
   if (consumer_list != NULL) {
